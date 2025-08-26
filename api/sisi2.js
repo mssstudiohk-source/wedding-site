c570e510d3479533cce022fe9529a389b6112557
mssstudiohk-source <mss.studio.hk@gmail.com>
2025-08-26 11:11:43 +0800

    Update sisi2.js

--- api/sisi2.js
+++ api/sisi2.js
@@ -1,5 +1,4 @@
-{
-  "functions": {
-    "api/*/.js": { "runtime": "nodejs20.x" }
-  }
+// api/sisi3.js  — 最簡 ESM 版本
+export default function handler(req, res) {
+  res.status(200).json({ ok: true, msg: "sisi3 ESM OK ✅" });
 }
